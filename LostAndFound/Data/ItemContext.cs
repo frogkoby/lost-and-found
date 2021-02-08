c813cd502d1602d992eab12b62164aaca0e4f4db
kobayashitatsuya <kobayashi@frog-pod.com>
2021-02-08 21:22:36 +0900

    やったぜ一覧地図が表示できました

--- LostAndFound/Data/ItemContext.cs
+++ LostAndFound/Data/ItemContext.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using LostAndFound.Models;
 
-    public class ItemContext : DbContext
+public class ItemContext : DbContext
     {
         public ItemContext (DbContextOptions<ItemContext> options)
             : base(options)
--- LostAndFound/Pages/Items/Index.cshtml.cs
+++ LostAndFound/Pages/Items/Index.cshtml.cs
@@ -6,23 +6,43 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using LostAndFound.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 
 namespace LostAndFound.Pages.Items
 {
     public class IndexModel : PageModel
     {
+
         private readonly ItemContext _context;
 
         public IndexModel(ItemContext context)
         {
             _context = context;
+
         }
 
         public IList<Item> Item { get;set; }
+
 
         public async Task OnGetAsync()
         {
             Item = await _context.Item.ToListAsync();
         }
     }
+    public static class JsonSerializer
+    {
+
+        public static JsonSerializerOptions Run()
+        {
+            return new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+                WriteIndented = true
+            };
+        }
+    }
+
 }
